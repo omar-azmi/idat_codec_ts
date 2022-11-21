06929b0b61da8552fca0b88d6a21f08b35422a8e
Omar Azmi <64020006+omar-azmi@users.noreply.github.com>
2022-11-21 20:42:26 +0300

    also export `deflate` and `inflate` to end-user use deno's `emit` library for bundling instead of esbuild use `esbuild` only for minification

--- build_dist.ts
+++ build_dist.ts
@@ -1,19 +1,21 @@
-import { build as esbuild, stop as esstop } from "https://deno.land/x/esbuild/mod.js"
-import { denoPlugin } from "https://deno.land/x/esbuild_deno_loader/mod.ts"
+import { transform as estransform, stop as esstop } from "https://deno.land/x/esbuild/mod.js"
+import { bundle } from "https://deno.land/x/emit/mod.ts"
 
 let t0 = performance.now(), t1: number
-await esbuild({
-	entryPoints: ["./src/mod.ts"],
-	outdir: "./dist/",
-	bundle: true,
-	minify: true,
-	platform: "neutral",
-	format: "esm",
-	target: "esnext",
-	plugins: [denoPlugin()],
-	define: {},
-})
+const
+	entrypoint = "./src/mod.ts",
+	outfile = "./dist/mod.min.js",
+	bundled = await bundle(entrypoint),
+	bundled_minified = await estransform(bundled.code, {
+		minify: true,
+		treeShaking: true,
+		drop: ["debugger", "console"],
+		format: "esm",
+		target: "esnext",
+		define: {},
+	})
 esstop()
 t1 = performance.now()
+Deno.writeTextFileSync(outfile, bundled_minified.code)
 console.log("execution time:", t1 - t0, "ms")
-console.log("dist binary size:", Deno.statSync("./dist/mod.js").size / 1024, "kb")
+console.log("dist binary size:", Deno.statSync(outfile).size / 1024, "kb")
